daf3c254a6ad3eef87064aa63f279fd456e3f1ca
Bahar Hosseini <bahar.hosseini@gmail.com>
2022-12-13 21:10:37 -0500

    setup navbar and homescreen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,21 @@
+//External Modules
+import { Container } from 'react-bootstrap';
+
+//Inner Modules
+import Footer from './components/Footer';
+import Header from './components/Header';
+import HomeScreen from './screens/HomeScreen';
+
 const App = () => {
   return (
     <>
-      <h1>Welcome to waterloo shop</h1>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <HomeScreen />
+          <Footer />
+        </Container>
+      </main>
     </>
   );
 };

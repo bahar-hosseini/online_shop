b1072f009649a21ec1756beea878239dff223405
Bahar Hosseini <bahar.hosseini@gmail.com>
2022-12-14 10:41:50 -0500

    using react-router in nav

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,23 +1,28 @@
 import React from 'react';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 
 const Header = () => {
   return (
     <header>
       <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
         <Container>
-          <Navbar.Brand href='/'>Waterloo shop</Navbar.Brand>
+          <LinkContainer to='/'>
+            <Navbar.Brand>Waterloo shop</Navbar.Brand>
+          </LinkContainer>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
-            <Nav className='ml-auto'>
-              <Nav.Link href='/cart'>
-                <i className='fas fa-shopping-cart'></i>
-              </Nav.Link>
-              <Nav.Link href='/login'>
-                <i className='fas fa-user'></i>Sign In
-              </Nav.Link>
+            <Nav className='ms-auto'>
+              <LinkContainer to='/cart'>
+                <Nav.Link>
+                  <i className='fas fa-shopping-cart'></i> Cart
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to='/login'>
+                <Nav.Link>
+                  <i className='fas fa-user'></i>Sign In
+                </Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,12 @@
+//External Modules
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
+
+//Internal Modules
+
+const ProductScreen = () => {
+  return <div>ProductScreen</div>;
+};
+
+export default ProductScreen;

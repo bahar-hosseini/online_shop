db005cfd6bd654235adeeaa995c194ae8afb5da4
Bahar Hosseini <bahar.hosseini@gmail.com>
2023-01-06 13:47:20 -0500

    organize code by adding controller directory

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,33 +1,15 @@
 //External Modules
 import express from 'express';
 const router = express.Router();
-import asyncHandler from 'express-async-handler';
 
 //Internal Modules
-import Product from '../models/productModel.js';
+import {
+  getProducts,
+  getProductById,
+} from '../controllers/productController.js';
 
-//* @describe: Fetch all products
-//* @route: Get /api/products
-//* @access Public route
-router.get(
-  '/',
-  asyncHandler(async (req, res) => {
-    const products = await Product.find({});
-    res.json(products);
-  })
-);
+router.route('/').get(getProducts);
 
-//* @describe: Fetch one specefic product
-//* @route: Get /api/products/:id
-//* @access: Public route
-router.get(
-  '/:id',
-  asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-
-    product ? res.json(product) : res.status(404);
-    throw new Error(`Product Not Found`);
-  })
-);
+router.route('/:id').get(getProductById);
 
 export default router;

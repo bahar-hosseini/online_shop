3f9b2574bd45fb37d4a4a80cf13148c19824f816
Bahar Hosseini <bahar.hosseini@gmail.com>
2023-01-10 12:25:45 -0500

    add authentication middleware and add user registeration

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,33 @@
+//External Modules
+import express from 'express';
+const router = express.Router();
+import asyncHandler from 'express-async-handler';
+
+//internal Modules
+import User from '../models/userModel.js';
+
+const protect = asyncHandler(async (req, res, next) => {
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    try {
+      token = rq.headers.authorization.split(' ')[1];
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = await User.findById(decoded.id).select('-password');
+      next();
+    } catch (error) {
+      console.error(error);
+      res.status(401);
+      throw new Error('Not authorized, token failed');
+    }
+  }
+
+  if (!token) {
+    res.status(401);
+    throw new Error('Not authorized, no token');
+  }
+});
+
+export { protect };

091624431edd83a7bd6e4a32d46c9610c76d935b
Bahar Hosseini <bahar.hosseini@gmail.com>
2023-01-12 23:46:24 -0500

    on client side setup login form page

---
+++ frontend/src/screens/LoginScreen.js
@@ -0,0 +1,64 @@
+//External Modules
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import Message from '../components/Message';
+import Loader from '../components/Loader';
+import { Form, Button, Row, Col } from 'react-bootstrap';
+
+//Internal Modules
+import FormContainer from '../components/FormContainer';
+
+const LoginScreen = ({ location, history }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const dispatch = useDispatch();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+  };
+  const redirect = location.search ? location.search.split('=')[1] : '/';
+  return (
+    <div>
+      <FormContainer>
+        <h1>LoginScreen</h1>
+        <Form onSubmit={submitHandler}>
+          <Form.Group controlId='email'>
+            <Form.Label>Email Address</Form.Label>
+            <Form.Control
+              type='email'
+              placeholder='Enter email'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+          <Form.Group controlId='password'>
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type='password'
+              placeholder='Enter password'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
+          <Button type='submit' variant='primary'>
+            Sign In
+          </Button>
+        </Form>
+        <Row className='py-3'>
+          <Col>
+            Haven't registerd?
+            <Link
+              to={redirect ? `/register?redirect=${redirect}` : '/register'}
+            >
+              Register
+            </Link>
+          </Col>
+        </Row>
+      </FormContainer>
+    </div>
+  );
+};
+
+export default LoginScreen;

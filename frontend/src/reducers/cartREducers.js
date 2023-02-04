63b0c15a1c352b54e1a72965768eb54f78cf71bc
Bahar Hosseini <bahar.hosseini@gmail.com>
2023-02-04 00:06:08 -0500

    add shipping form and handle its  states

--- frontend/src/reducers/cartREducers.js
+++ frontend/src/reducers/cartREducers.js
@@ -1,4 +1,8 @@
-import { CART_ADD_ITEM, CART_REMOVE_ITEM } from '../constants/cardConstant';
+import {
+  CART_ADD_ITEM,
+  CART_REMOVE_ITEM,
+  CART_SAVE_SHIPPING_ADDRESS,
+} from '../constants/cartConstant';
 
 export const cartReducer = (
   state = { cartItems: [], shippingAddress: {} },
@@ -26,6 +30,11 @@ export const cartReducer = (
       return {
         ...state,
         cartItems: state.cartItems.filter((x) => x.product !== action.payload),
+      };
+    case CART_SAVE_SHIPPING_ADDRESS:
+      return {
+        ...state,
+        shippingAddress: action.payload,
       };
     default:
       return state;

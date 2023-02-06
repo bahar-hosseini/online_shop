655d5f91a65696b6e1fde13a1daa78a030a72bd5
Bahar Hosseini <bahar.hosseini@gmail.com>
2023-02-06 05:12:36 -0500

    change css style

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,13 +13,14 @@ import RegisterScreen from './screens/RegisterScreen';
 import ProfileScreen from './screens/ProfileScreen';
 import ShippingScreen from './screens/ShippingScreen';
 import PaymentScreen from './screens/PaymentScreen';
+import PlaceOrderScreen from './screens/PlaceOrderScreen';
 
 const App = () => {
   return (
     <Router>
       <Header />
       <main>
-        <Container className='p-0'>
+        <Container className='p-0' fluid>
           <Route path='/login' component={LoginScreen} />
           <Route path='/register' component={RegisterScreen} />
           <Route path='/profile' component={ProfileScreen} />
@@ -27,6 +28,7 @@ const App = () => {
           <Route path='/cart/:id?' component={CartScreen} />
           <Route path='/shipping' component={ShippingScreen} />
           <Route path='/payment' component={PaymentScreen} />
+          <Route path='/placeorder' component={PlaceOrderScreen} />
           <Route path='/' component={HomeScreen} exact />
 
           <Footer />
